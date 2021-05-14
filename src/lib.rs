b7658a2378f2db0ba2596015da6075025b7ef0c2
MURAKI Taichi <muraki.taichi@gmail.com>
2021-05-14 16:03:50 +0900

    Fix to try pass image data from js.

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,10 @@ extern {
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
 }
+
+#[wasm_bindgen(js_name = processImage)]
+pub fn process_image(data: Vec<u8>) {
+   //let result = image::load_from_memory_with_format(&data, image::ImageFormat::PNG);
+
+
+}
